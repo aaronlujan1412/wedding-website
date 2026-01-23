f60c3539eac15f27a8ab2b114811d30a8bdb281e
aaron.lujan1412 <aaron.lujan1412@gmail.com>
2026-01-23 01:04:03 -0700

    feat: adding modal to front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 import { Corinthia } from "next/font/google";
+import RsvpModal from "@/components/rsvp/RsvpModal";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 const corinthia = Corinthia({
   subsets: ["latin"],
@@ -10,18 +14,18 @@ const corinthia = Corinthia({
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-stone-100">
       <h1 className={`${corinthia.variable} text-6xl font-corinthia text-stone-800`}>
-        Aaron & Savea's Wedding
+        <div>Aaron</div>
+        <div className="text-center">&</div>
+        <div>Savea</div>
       </h1>
       <p>
         (Because who else would it be?)
       </p>
       <p className="mt-4 text-xl text-stone-600">
-        October 2025 • Save the Date
+        12 ·  01 · 2025
       </p>
 
-      <button className="mt-8 px-6 py-3 bg-stone-900 text-white rounded-lg hover:bg-stone-700 transition">
-        RSVP Now
-      </button>
+      <RsvpModal />
     </main>
   );
 }
