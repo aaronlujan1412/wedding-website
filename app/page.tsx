df060903ecb5a01ef658a11f96cea2ef250acd9e
aaronlujan1412 <aaron.lujan1412@gmail.com>
2026-02-24 18:00:40 -0700

    feat: make front page column - add shading to background for text visibility

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,30 @@
-import { Corinthia } from "next/font/google";
 import RsvpModal from "@/components/rsvp/RsvpModal";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
+import { maxHeaderSize } from "http";
+import Image from "next/image";
 
-const corinthia = Corinthia({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-corinthia",
-});
-
-  export default function Home() {
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-stone-100">
-      <h1 className={`${corinthia.variable} text-6xl font-corinthia text-stone-800`}>
-        <div>Aaron</div>
-        <div className="text-center">&</div>
-        <div>Savea</div>
-      </h1>
-      <p>
-        (Because who else would it be?)
-      </p>
-      <p className="mt-4 text-xl text-stone-600">
-        12 ·  01 · 2025
-      </p>
+    <main className="flex flex-col">
+      <div className="relative w-full h-screen">
+        <Image
+          src={"/media/HuggingInForest.jpg"}
+          fill
+          className="object-cover"
+          alt="Aaron and Savea in a forest"
+        ></Image>
+        <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-transparent" />
+      </div>
+      <div className="flex min-h-screen flex-col items-center justify-center p-24">
+        <h1 className="text-6xl font-corinthia text-stone-800">
+          <div>Aaron</div>
+          <div className="text-center">&</div>
+          <div>Savea</div>
+        </h1>
+        <p>(Because who else would it be?)</p>
+        <p className="mt-4 text-xl text-stone-600">12 · 01 · 2025</p>
 
-      <RsvpModal />
+        <RsvpModal />
+      </div>
     </main>
   );
 }
