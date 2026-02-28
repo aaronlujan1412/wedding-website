ac37bb668c51af2ed6b803246b133f75000ba37f
aaronlujan1412 <aaron.lujan1412@gmail.com>
2026-02-28 13:45:40 -0700

    feat: add logo to navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -5,12 +5,13 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { MenuIcon } from "lucide-react";
 import { NAV_ITEMS } from "./NavBarItems";
+import Image from "next/image";
 
 export function Navbar() {
   return (
-    <nav className="text-primary-foreground flex fixed top-0 left-0 right-0 z-50 w-full max-w-full">
+    <nav className="text-primary-foreground flex fixed top-15 left-0 right-0 z-50 w-full max-w-full font-raleway">
       {/* Left */}
-      <div className="gap-5 flex flex-1 justify-end items-center">
+      <div className="gap-5 flex flex-1 justify-evenly items-center">
         {NAV_ITEMS.filter((i) => i.position === "left").map((link) => (
           <Link className="px-4 py-2" key={link.href} href={link.href}>
             {link.label}
@@ -18,15 +19,17 @@ export function Navbar() {
         ))}
       </div>
       {/* Middle */}
-      <div className="text-8xl">
-        {NAV_ITEMS.filter((i) => i.position === "center").map((link) => (
-          <Link className="font-corinthia" key={link.href} href={link.href}>
-            {link.label}
-          </Link>
-        ))}
-      </div>
+      <Link href={"/"}>
+        <Image
+          src={"/media/LogoWhite.png"}
+          width={300}
+          height={150}
+          alt="Logo of initials."
+          className="w-auto h-auto max-w-24"
+        ></Image>
+      </Link>
       {/* Right */}
-      <div className="gap-5 flex flex-1 justify-start items-center">
+      <div className="gap-5 flex flex-1 justify-evenly items-center">
         {NAV_ITEMS.filter((i) => i.position === "right").map((link) => (
           <Link className="px-4 py-2" key={link.href} href={link.href}>
             {link.label}
