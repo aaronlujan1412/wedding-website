83e6fab199e9e6ce378581a5f7224f5bb9432ffa
aaronlujan1412 <aaron.lujan1412@gmail.com>
2026-03-08 14:46:10 -0600

    chore: add address type for rsvp

--- components/rsvp/types.ts
+++ components/rsvp/types.ts
@@ -2,3 +2,14 @@ import type { Database } from "@/lib/database.types";
 
 export type Guest = Database["public"]["Tables"]["guests"]["Row"];
 export type GuestGroup = Database["public"]["Tables"]["guest_groups"]["Row"];
+export interface Address {
+  addressStreet: string;
+  addressCity: string;
+  addressState: string;
+  addressZip: string;
+}
+export interface RsvpFormData {
+  groupId: Number;
+  groupMembers: Guest[];
+  address: Address | null;
+}
