c35555bb83382492c8cc9f56bb4a1cb7574429c6
aaronlujan1412 <aaron.lujan1412@gmail.com>
2026-03-08 15:27:32 -0600

    feat: add method to fetch guests from group id

--- app/actions/rsvp.ts
+++ app/actions/rsvp.ts
@@ -11,3 +11,8 @@ export async function getAllGuestGroups() {
   const data = await supabase.from("guest_groups").select();
   return data;
 }
+
+export async function getGuestsFromGroupId(groupId: number) {
+  const data = await supabase.from("guests").select().eq("group_id", groupId);
+  return data;
+}
