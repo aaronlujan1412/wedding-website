603d0882a958f46964c606b9cf5ab728767e3999
aaronlujan1412 <aaron.lujan1412@gmail.com>
2026-03-08 15:30:26 -0600

    chore: text change

--- components/rsvp/RsvpStepTwo.tsx
+++ components/rsvp/RsvpStepTwo.tsx
@@ -10,7 +10,9 @@ export default function RsvpStepTwo({ groupMembers }: Props) {
   return (
     <div className="grid gap-4 py-4">
       <div className="grid gap-2">
-        <Label htmlFor="group">Which of your party will be attending?</Label>
+        <Label htmlFor="group">
+          Please provide all details for associated members:
+        </Label>
         {groupMembers.map((member, id) => (
           <div key={member.id} className="flex items-center gap-2">
             <input
